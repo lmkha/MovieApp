b5afd99a3b09cac108986e4769f5300bcdd7005a
Le Minh Kha <lmkha0201@gmail.com>
2024-11-22 04:26:12 +0700

    first commit

---
+++ app/src/main/java/com/piashcse/hilt_mvvm_compose_movie/data/datasource/remote/ApiURL.kt
@@ -0,0 +1,6 @@
+package com.piashcse.hilt_mvvm_compose_movie.data.datasource.remote
+
+object ApiURL {
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val IMAGE_URL = "https://image.tmdb.org/t/p/w342"
+}
